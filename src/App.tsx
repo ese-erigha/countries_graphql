eae7eb39c1fde776389cb8073ee61af83d49dd6a
Eseoghene Erigha <ese.erigha@gmail.com>
2021-01-28 22:36:41 +0000

    nits

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
 import React, { lazy, Suspense } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Layout from './components/Layout';
 
 const CountryList = lazy(() => import('./CountryList'));
 const CountryDetail = lazy(() => import('./CountryDetail'));
 
-const App = (): JSX.Element => {
-  return (
-    <Layout>
-      <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/countries" />
-            </Route>
-            <Route path="/countries" exact component={CountryList} />
-            <Route path="/countries/:id" component={CountryDetail} />
-          </Switch>
-        </Suspense>
-      </Router>
-    </Layout>
-  );
-};
+const App = (): JSX.Element => (
+  <Layout>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/countries" />
+          </Route>
+          <Route path="/countries" exact component={CountryList} />
+          <Route path="/countries/:id" component={CountryDetail} />
+        </Switch>
+      </Suspense>
+    </Router>
+  </Layout>
+);
 
 export default App;
